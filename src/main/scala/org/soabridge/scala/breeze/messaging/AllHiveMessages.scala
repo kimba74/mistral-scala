27cefb29ad5110bdf9608249aeb9359b257dff98
Steffen Krause <steffen.krause@soabridge.com>
2015-03-29 12:51:25 -0400

    Added top-level message parent trait to development branch

---
+++ src/main/scala/org/soabridge/scala/breeze/messaging/AllHiveMessages.scala
@@ -0,0 +1,11 @@
+package org.soabridge.scala.breeze.messaging
+
+/**
+ * This trait is the top parent of all Hive Messages exchanged on the bus.
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+trait AllHiveMessages {
+
+}
