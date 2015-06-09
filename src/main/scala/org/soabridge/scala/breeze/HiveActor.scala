f626fb6571121beca72ab87fea59ef7007d6b934
Steffen Krause <steffen.krause@soabridge.com>
2015-06-08 22:23:12 -0400

    Added initial base implementation for Hive actor.

--- src/main/scala/org/soabridge/scala/breeze/HiveActor.scala
+++ src/main/scala/org/soabridge/scala/breeze/HiveActor.scala
@@ -1,6 +1,9 @@
 package org.soabridge.scala.breeze
 
-import akka.actor.{Props, Actor}
+import akka.actor.SupervisorStrategy.Resume
+import akka.actor._
+import org.soabridge.scala.breeze.modules.ModulesActor
+import org.soabridge.scala.breeze.resources.ResourceActor
 
 /**
  * Missing documentation. 
@@ -12,9 +15,21 @@ private[breeze] class HiveActor extends Actor {
   /* Importing all messages declared in companion object for processing */
   import HiveActor.Messages._
 
+  /** Supervising actor for all resources in the hive */
+  val resActor = context.watch(context.actorOf(ResourceActor.props, "resources"))
+  /** Supervising actor for all configured module instances */
+  val modActor = context.watch(context.actorOf(ModulesActor.props, "modules"))
+
+  /** */
+  override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+    //TODO slk: implement supervisor strategy
+    case Exception => Resume
+  }
+
   /** Message processing */
   def receive: Receive = {
     case Status =>
+    case Terminated =>
   }
 }
 
@@ -30,6 +45,8 @@ private[breeze] object HiveActor {
 
   /** Accepted messages for HiveActor */
   object Messages {
-    case class Status
+    case object Start
+    case object Status
+    case object Stop
   }
 }
