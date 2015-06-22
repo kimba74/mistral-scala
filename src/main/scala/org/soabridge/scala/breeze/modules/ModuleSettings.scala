c6a854b996e6268a2b90fed7c503b7d097dffa91
Steffen Krause <steffen.krause@soabridge.com>
2015-06-21 22:18:10 -0400

    First Test of ModuleSettings returning classes for Worker- and Mailbox Type

--- src/main/scala/org/soabridge/scala/breeze/modules/ModuleSettings.scala
+++ src/main/scala/org/soabridge/scala/breeze/modules/ModuleSettings.scala
@@ -1,6 +1,8 @@
 package org.soabridge.scala.breeze.modules
 
+import akka.actor.Actor
 import com.typesafe.config.Config
+import scala.reflect.runtime.{universe => ru}
 
 /**
  * Missing documentation. 
@@ -12,11 +14,22 @@ class ModuleSettings(val conf: Config, final val name: String) {
 
   final val mailboxParams = conf.getConfig("mailbox.params")
 
-  final val mailboxType = conf.getString("mailbox.type")
+  final val mailboxType = Class.forName(conf.getString("mailbox.type")) // TODO slk: figure out reflection in Scala
 
   final val workerParams = conf.getString("worker.params")
 
   final val workerPoolSize = conf.getInt("worker.pool-size")
 
-  final val workerType = conf.getString("worker.type")
+  final val workerType = Class.forName(conf.getString("worker.type"))   // TODO slk: figure out reflection in Scala
+}
+
+/**
+ * Missing documentation.
+ *
+ * @author <a href="steffen.krause@soabridge.com">Steffen Krause</a>
+ * @since 1.0
+ */
+object ModuleSettings {
+
+  def apply(conf: Config): ModuleSettings = new ModuleSettings(conf, conf.getString("name"))
 }
