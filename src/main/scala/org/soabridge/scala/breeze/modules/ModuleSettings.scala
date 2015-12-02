e72eb18d07460cb4a0e45c7fe47b64c17bc5cbc0
Steffen Krause <steffen.krause@soabridge.com>
2015-12-01 21:15:50 -0500

    Added ScalaDoc to the ModuleSettings class.

--- src/main/scala/org/soabridge/scala/breeze/modules/ModuleSettings.scala
+++ src/main/scala/org/soabridge/scala/breeze/modules/ModuleSettings.scala
@@ -13,18 +13,31 @@ class ModuleSettings(val conf: Config, final val name: String) {
 
   private def validate(config: Config):Boolean = true // TODO slk: implement validation logic
 
+  /**
+   * Returns a sequence of event classes the worker pool will register with
+   * on the event stream.
+   */
   final val events: Seq[Class[_]] = {
     //conf.get...
     Array(classOf[String])
   }
 
+  /**
+   * Returns the class of the worker implementation the pool will be filled with.
+   */
   final val workerClass: Class[_] = Class.forName(conf.getString("worker.class"))
 
+  /**
+   * Returns a sequence of parameters passed to the worker's constructor.
+   */
   final val workerParams: Seq[Any] = {
     //conf.getString("worker.params")
     Array("Test")
   }
 
+  /**
+   * Returns the pool size of the worker pool.
+   */
   final val poolSize: Int = conf.getInt("worker.pool-size")
 }
 
